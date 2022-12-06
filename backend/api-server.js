fd015e6d26737375a8692bb85fff79bbf0e383bb
heeezin <107285791+heeezin@users.noreply.github.com>
2022-12-06 19:15:10 +0900

    memo

---
+++ backend/api-server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require("body-parser");
+
+const memos = [];
+
+app.use(bodyParser.json());
+
+app.get('/api/memos', (req, res) => {
+  res.send(memos)
+})
+app.post("/api/memos", (req,res) => {
+    // console.log(req.body)
+    memos.push(req.body.content);
+    res.send(memos);
+});
+app.put("/api/memos/:idx", (req,res) => {
+  memos[req.params.idx] = req.body.content;
+  res.send(memos);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`)
+})
---
+++ src/components/Memo.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="memo">
+        <div class="act">
+            <button class="btn btn-primary btn-block" @click="add()">+ 추가</button>
+        </div>
+        <ul>
+            <li v-for="(d, idx) in state.data" :key="idx" @click="edit(idx)">{{ d }}</li>
+        </ul>
+    </div>
+</template>
+<script>
+import { reactive } from 'vue';
+import axios from "axios";
+
+export default {
+    setup() {
+        const state = reactive({
+            data:[]
+        })
+        const add = ()=> {
+            const content = prompt("내용을 입력해주세요.");
+            axios.post("/api/memos", { content}).then((res)=>{
+                state.data = res.data;
+            })
+        };
+        const edit = (idx) => {
+            const content = prompt("내용을 입력해주세요.", state.data[idx]);
+
+            axios.put("/api/memos/" + idx, {content}).then((res)=> {
+                state.data = res.data;
+            });
+        };
+        axios.get("/api/memos").then((res)=>{
+            state.data = res.data;
+        })
+        return { state, add, edit };
+    }
+}
+</script>
+<style lang="scss" scoped>
+.memo {
+    .act {
+        text-align: right;
+        padding: 10px 10px 5px 5px;
+    }
+    ul {
+        list-style: none;
+        padding: 15px 0;
+        margin: 0;
+        li {
+            padding: 15px;
+            margin: 10px 0;
+            border:1px solid #eee;
+        }
+    }
+}
+</style>
